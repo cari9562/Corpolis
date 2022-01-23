ee2bf13f7cb10f10f124ec31fd7250ef352e8747
cari9562 <kerry.qianqi@gmail.com>
2022-01-23 15:05:03 +0100

    Asset upload

---
+++ Assets/Resources/Informations/AllItems/Item.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public string description;
+    public Sprite icon;
+
+    public int cost;
+    public int hunger;
+    public int thirsty;
+    public int health;
+    public int mental;
+    public int attack;
+    public int number;
+
+    public int GetCost()
+    {
+        return cost;
+    }
+
+    public int GetHunger()
+    {
+        return hunger;
+    }
+
+    public int GetThirsty()
+    {
+        return thirsty;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public int GetNumber()
+    {
+        return number;
+    }
+
+    public Sprite GetIcon()
+    {
+        return icon;
+    }
+}
---
+++ Assets/Resources/Informations/AllItems/ItemImg.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemImg : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Image icon;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        icon = GetComponent<Image>();
+    }
+}
