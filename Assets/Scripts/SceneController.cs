8746d7654e69ef1f53a8191af1720943f4215a34
cari9562 <kerry.qianqi@gmail.com>
2022-01-24 21:05:01 +0100

    Asset bundles test done to load scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneController", menuName = "SceneObj")]
+public class SceneController : ScriptableObject
+{
+//#if UNITY_EDITOR
+//    [SerializeField] private List<SceneAsset> _scenes;
+//    private Dictionary<string, SceneAsset> _sceneDic = new Dictionary<string, SceneAsset>();
+//#endif
+
+//    private void OnEnable()
+//    {
+//        foreach (SceneAsset scene in _scenes)
+//        {   
+//            _sceneDic.Add(scene.name, scene);
+//            Debug.Log("scene name: " + scene.name + "\n" + "scene: " + scene);
+//        }
+//    }
+
+//    public SceneAsset GetSceneByName(string sceneName)
+//    {
+//        return _sceneDic[sceneName];
+//    }
+}
+
---
+++ Assets/Scripts/SingletonObj.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonObj : MonoBehaviour
+{
+    private static SingletonObj _instance;
+
+    public static SingletonObj Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
